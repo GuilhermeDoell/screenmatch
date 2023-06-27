c705a62e1345029af39ce2cb222cf2babdfb4889
Guilherme Doell <guilhermedoell@outlook.com>
2023-06-26 21:03:09 -0300

    Add files via upload

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,45 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.*;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(9);
+        Serie lost = new Serie("Lost", 2000);
+        Filme outroFilme = new Filme("Avatar", 2023);
+        outroFilme.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville", 2023);
+        filmeDoPaulo.avalia(10);
+
+        Filme f1 = filmeDoPaulo;
+
+        List<Titulo> lista = new LinkedList<>();
+        lista.add(filmeDoPaulo);
+        lista.add(meuFilme);
+        lista.add(outroFilme);
+        lista.add(lost);
+        for (Titulo item: lista) {
+            System.out.println(item);
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação " + filme.getClassificacao());
+            }
+        }
+
+        ArrayList<String> buscaPorArtista = new ArrayList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("João");
+        buscaPorArtista.add("Guilherme Doell");
+        Collections.sort(buscaPorArtista); //ordena por ordem alfabética
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(lista);
+        System.out.println(lista);
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando filmes e séries por ano: " + lista);
+    }
+}
